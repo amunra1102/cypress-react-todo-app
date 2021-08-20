02e91056b7be55e532ad1b044d57f8d434199f43
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-08-21 00:29:59 +0700

    config docker

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM cypress/base:12
+
+COPY . .
+
+RUN ["npm", "install"]
+
+RUN ["npm", "run", "build:and:test"]
--- cypress/integration/ui/filter.spec.js
+++ cypress/integration/ui/filter.spec.js
@@ -15,27 +15,27 @@ describe('Filter functionality test cases', () => {
   });
 
   it('should filter the completed todos correctly', () => {
-    // cy.contains('Complete').click();
-    // cy.url().should('contain', '/complete');
-    // cy.get('.todo-checkbox').each(el => {
-    //   cy.wrap(el).should('be.checked');
-    // });
+    cy.contains('Complete').click();
+    cy.url().should('contain', '/complete');
+    cy.get('.todo-checkbox').each(el => {
+      cy.wrap(el).should('be.checked');
+    });
   });
 
   it('should filter the active todos correctly', () => {
-    // cy.contains('Active').click();
-    // cy.url().should('contain', '/active');
-    // cy.get('.todo-checkbox').each(el => {
-    //   cy.wrap(el).should('not.be.checked');
-    // });
+    cy.contains('Active').click();
+    cy.url().should('contain', '/active');
+    cy.get('.todo-checkbox').each(el => {
+      cy.wrap(el).should('not.be.checked');
+    });
   });
 
   after(() => {
-    // cy.get('body').then($element => {
-    //   if ($element.find('.delete-item').length) {
-    //     // cy.wait(10000);
-    //     cy.get('.delete-item').click({ multiple: true });
-    //   }
-    // });
+    cy.get('body').then($element => {
+      if ($element.find('.delete-item').length) {
+        cy.wait(10000);
+        cy.get('.delete-item').click({ multiple: true });
+      }
+    });
   });
 });
